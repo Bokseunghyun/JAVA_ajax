47d27db50349d0150a64dd8b9d3ac60196b4ce0b
Bokseunghyun <53068378+Bokseunghyun@users.noreply.github.com>
2019-07-19 12:07:43 +0900

    Create ajaxBordListView.js

---
+++ ajax_mysql_Project/WebContent/js/ajaxBordListView.js
@@ -0,0 +1,28 @@
+function bordListView(){
+	var mId = document.getElementById("mId");
+	var url = "BordView.bo";
+	var data ="mId="+mId.value;
+	
+	xhr.open("post",url,true);
+	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded;charset=utf8');
+	
+	xhr.onreadystatechange=function(){
+		if(xhr.status==200 && xhr.readyState==4){
+			var rs = JSON.parse(xhr.responseText);
+			console.log(xhr.responseText);
+			console.log(rs);
+			if(rs!=null){
+				document.getElementById("mGroup").textContent=rs.result1.mGroup;
+				document.getElementById("hit").textContent=rs.result1.hit;
+				document.getElementById("mIndent").textContent=rs.result1.mIndent;
+				document.getElementById("mTime").textContent=rs.result1.mTimestamp;
+				document.getElementById("nickName").value=rs.result1.nickName;
+				document.getElementById("userId").value=rs.result1.userId;
+				document.getElementById("title").value=rs.result1.title;
+				document.getElementById("mPw").value=rs.result1.mPw;
+				document.getElementById("memo").value=rs.result1.memo;
+			}
+		}
+	}
+	xhr.send(data);
+}
